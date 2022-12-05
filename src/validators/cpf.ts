9adb8b0cc8de156129d90a366879f7fd3f2678b1
Davi Viana <davianasales@outlook.com>
2022-12-05 03:17:58 -0300

    feat(validators): cpf & cnpj

---
+++ src/validators/cpf.ts
@@ -0,0 +1,59 @@
+import { DocumentValidatorComparer, Validator } from "."
+
+export class CpfDocument implements DocumentValidatorComparer {
+  getDocumentValue() {
+    return this.value.substring(this.startIndex, this.endIndex + 1)
+  }
+  getDocumentVerification() {
+    return this.value.substring(this.verificationDigitsIndex)
+  }
+
+  startIndex: number = 0
+  endIndex: number = 8
+  verificationDigitsIndex: number = 9
+
+  private value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+}
+
+export const cpfValidationHandler: Validator = async (value) => {
+  const cDoc = new CpfDocument(value.toString())
+
+  let checkBuff = []
+
+  function reduceBuffer() {
+    const reduced = checkBuff.reduce((a, b) => a + b)
+    checkBuff = []
+    return reduced
+  }
+
+  function resolveReducedBuffer(value: any) {
+    return value % 11 >= 10 ? 0 : value % 11
+  }
+
+  for (const { index, value } of Array.from(cDoc.getDocumentValue()).map(
+    (value, index) => ({
+      value,
+      index,
+    })
+  )) {
+    checkBuff.push(Number(value) * (index + 1))
+  }
+
+  let reducedBuff = reduceBuffer()
+  let verification0 = resolveReducedBuffer(reducedBuff)
+
+  for (const { index, value } of Array.from(
+    cDoc.getDocumentValue() + verification0.toString()
+  ).map((value, index) => ({ value, index }))) {
+    checkBuff.push(Number(value) * index)
+  }
+
+  reducedBuff = reduceBuffer()
+  let verification1 = resolveReducedBuffer(reducedBuff)
+
+  return `${verification0}${verification1}` === cDoc.getDocumentVerification()
+}
